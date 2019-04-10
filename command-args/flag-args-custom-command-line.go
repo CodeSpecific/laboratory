43122fabe8ff0d7e62402f104306a46361d14a6f
bitsman <3485537725@qq.com>
2019-04-10 19:53:02 +0800

    通过查看源码，通过flag.CommandLine和自定义Flag.FlagSet实例的方式定制命令参数容器，来更好的解释说明参数的用法

---
+++ command-args/flag-args-custom-command-line.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+var name string
+
+// CommandLine其实就是flag包中一个全局变量，默认实现返回了FlagSet的实例
+// 跟自己创建实例没有任何影响，这样可以更加灵活的定制命令参数容器
+// 例子中的实现完全可以参考源代码中默认Command的实现
+var cm *flag.FlagSet
+
+func init() {
+	cm = flag.NewFlagSet("question", flag.ExitOnError)
+	cm.StringVar(&name, "name", "vitamin", "everyone`s name")
+	cm.Usage = func() {
+		fmt.Fprintf(os.Stdout, "Usage of %s:\n", "question")
+		cm.PrintDefaults()
+	}
+}
+
+func main() {
+	cm.Parse(os.Args[1:])
+	fmt.Printf("Hello,%s\n", name)
+}
---
+++ command-args/flag-args-custom-usage.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+var name string
+
+func init() {
+	// 使用CommandLine进行定制化Usage，其实flag.Usage底层的方法也是使用的CommandLine
+	// CommandLine是默认情况下的参数容器
+	flag.CommandLine = flag.NewFlagSet("", flag.ExitOnError)
+	flag.StringVar(&name, "name", "vitamin", "everyone` name")
+	flag.CommandLine.Usage = func() {
+		fmt.Fprintf(os.Stdout, "Usage of %s:\n", "question")
+		flag.PrintDefaults()
+	}
+}
+
+func main() {
+	flag.Parse()
+	fmt.Printf("hello,%s \n", name)
+}
